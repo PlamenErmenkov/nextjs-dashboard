019b1fb185e3e4fc73bac603e0d90574504c4332
PlamenErmenkov <116002627+PlamenErmenkov@users.noreply.github.com>
2024-11-06 15:01:47 +0200

    added data fetching for the dashboard page

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -48,7 +48,7 @@ export async function fetchLatestInvoices() {
   }
 }
 
-export async function fetchCardData() {
+export async function fetchCardData(p0: { LatestInvoices(): any; }) {
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
